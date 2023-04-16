18819729f114690d1843a5251f33270906ecd78d
Filipe Merli <filipe.merli@farfetch.com>
2023-04-16 18:29:17 +0100

    bunsdssfs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 /*==================== MENU SHOW AND HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
 	  navToggle = document.getElementById('nav-toggle'),
-	  navClose = document.getElementById('nav-close');
+	  navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
 	navToggle.addEventListener('click', () =>{
-		navMenu.classList.add('show-menu');
+		navMenu.classList.add('show-menu')
 	});
 } 
 
@@ -15,18 +15,18 @@ if(navToggle){
 /* Validate if constant exists */
 if(navClose){
 	navClosen.addEventListener('click', () =>{
-		navMenu.classList.remove('show-menu');
-	});
+		navMenu.classList.remove('show-menu')
+	})
 }
 
 /*==================== REMOVE MENU MOBILE ===========  =========*/
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-	const navMenu = document.getElementById('nav-menu');
-	navMenu.classList.remove('show-menu');
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
